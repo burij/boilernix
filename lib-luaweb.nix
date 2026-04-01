0cca487a892171a0bac1bd4fc2201ec8125258d0
Michael Burij <michael.burij@gmail.com>
2026-04-01 11:21:52 +0200

    Update Nix configuration for luaweb package

--- lib-luaweb.nix
+++ lib-luaweb.nix
@@ -1,11 +1,13 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import
+    (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11")
+    { config = { }; overlays = [ ]; }
+}:
 
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.05";
-  pkgs = import nixpkgs { config = { }; overlays = [ ]; };
-
+  lib = pkgs.lib;
   appName = "lahna";
-  appVersion = "0.9";
+  appVersion = lib.strings.fileContents ./VERSION;
+  appPort = 8152;
 
   luaEnv = pkgs.luajit.withPackages (ps: with ps; [
     luarocks
@@ -18,23 +20,23 @@ let
     pandoc
   ];
 
+  luaLightWings = {
+    url = "https://raw.githubusercontent.com/burij/"
+      + "lua-light-wings/refs/tags/v.0.4/modules/lua-light-wings.lua";
+    sha256 = "sha256-Tczj+XNIobX64Cncm0/rbDwMizUDhRmeyjFwrJrDCco=";
+  };
+
   shell = pkgs.mkShell {
     buildInputs = [ luaEnv dependencies ];
     shellHook = ''
       # export LUAOS="./conf.lua"
       alias run='lua main.lua'
       alias lahna='./result/bin/lahna'
-      alias build='nix-build -A package'
-      alias make='rm result;git add .;build;git commit -m '
+      alias form='nixpkgs-fmt lib.nix'
       mkdir modules
 
-      cp ${pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/burij/"
-          +"lua-light-wings/refs/tags/v.0.3.1/modules/lua-light-wings.lua";
-        sha256 = "sha256-3RBZDxmSOXNJ+rpBG/VmXSajTRsvVU2y8zKsxqDw4JA=";
-      }} ./modules/lua-light-wings.lua
+      cp ${pkgs.fetchurl luaLightWings} ./modules/lua-light-wings.lua
 
-      nixpkgs-fmt default.nix
     '';
   };
 
@@ -42,20 +44,16 @@ let
     pname = appName;
     version = appVersion;
 
-    # src = ./.;
+    src = ./.;
 
-    src = pkgs.fetchFromGitHub {
-      owner = "burij";
-      repo = appName;
-      rev = appVersion;
-      sha256 = "sha256-un77btYtBtciIc761pBpoYhxGGPodIVlnAdr2x6duBo=";
-    };
+    # src = pkgs.fetchFromGitHub {
+    #   owner = "burij";
+    #   repo = appName;
+    #   rev = appVersion;
+    #   sha256 = "";
+    # };
 
-    extraFile = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/burij/"
-        + "lua-light-wings/refs/tags/v.0.3.1/modules/lua-light-wings.lua";
-      sha256 = "sha256-3RBZDxmSOXNJ+rpBG/VmXSajTRsvVU2y8zKsxqDw4JA=";
-    };
+    extraFile = pkgs.fetchurl luaLightWings;
 
     nativeBuildInputs = [ pkgs.makeWrapper ];
     buildInputs = [ luaEnv dependencies ];
@@ -89,5 +87,64 @@ let
       platforms = platforms.all;
     };
   };
+
+  container = { config, lib, pkgs, ... }: {
+    containers.${appName} = {
+      autoStart = true;
+      privateNetwork = false;
+      privateUsers = "no";
+      hostAddress = "10.0.0.1";
+      localAddress = "10.0.0.2";
+
+      bindMounts = {
+        "${appName}-public" = {
+          hostPath = "/srv/config/${appName}/public";
+          mountPoint = "/var/lib/${appName}/public";
+          isReadOnly = false;
+        };
+        "${appName}-conf" = {
+          hostPath = "/srv/config/${appName}/conf.lua";
+          mountPoint = "/var/lib/${appName}/conf.lua";
+          isReadOnly = false;
+        };
+      };
+
+      config = { config, pkgs, ... }: {
+        system.stateVersion = "25.11";
+
+        environment.systemPackages = with pkgs; [
+          package
+        ];
+
+        systemd.services."${appName}" = {
+          description = "${appName}-daemon";
+          after = [ "network.target" ];
+          environment = {
+            LAHNA_HOST = "0.0.0.0";
+            LAHNA_PORT = "${toString appPort}";
+          };
+          serviceConfig = {
+            Type = "simple";
+            ExecStart = "${package}/bin/${appName} /var/lib/${appName}/conf.lua";
+            Restart = "always";
+            RestartSec = 10;
+            StandardOutput = "journal";
+            StandardError = "journal";
+            WorkingDirectory = "${package}/lib/${appName}";
+          };
+          wantedBy = [ "multi-user.target" ];
+        };
+
+        users.users.${appName} = {
+          isSystemUser = true;
+          group = appName;
+        };
+        users.groups.${appName} = { };
+
+        networking.firewall.allowedTCPPorts = [ appPort ];
+      };
+    };
+  };
+
 in
-{ shell = shell; package = package; }
+{ shell = shell; package = package; container = container; }
